74a544b2bd2247beee302a29d6fca3ef509bfc4e
Schnippen <dfojcik35@gmail.com>
2023-11-18 14:11:09 +0100

    working on Pagination

---
+++ app/_components/Pagination/Pagination.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import React from "react";
+
+function Pagination() {
+     const router = useRouter();
+     const searchParams = useSearchParams();
+  return <nav>Pagination</nav>;
+}
+
+export default Pagination;
