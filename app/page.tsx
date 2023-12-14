21517894945f831c79936c27610c54cf1b33c9b8
Schnippen <dfojcik35@gmail.com>
2023-12-14 21:29:17 +0100

    added text to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,31 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import backgroundImg from "../public/images/backgroundPhoto2.jpg";
 import SearchForm from "./_components/SearchForm/SearchForm";
-import LandingPageBackground from "../public/images/LandingPageBackground.jpg";
+import LandingPageBackground from "../public/images/LandingPageBackground.webp";
 
 export default function Home() {
+  const LandingPageDescription = () => {
+    return (
+      <div className={styles.DescriptionWrapper}>
+        <div className={styles.DescriptionContainer}>
+          <h1 className={styles.DescriptionContainer_title}>
+            Find your most Suitable Property
+          </h1>
+          <p className={styles.DescriptionContainer_paragraph}>
+            Explore a diverse portfolio of properties, from cozy apartments to
+            sprawling estates, and find the perfect place to call home
+          </p>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <main className={styles.main}>
       <section className={styles.landing__page_section}>
         <div className={styles.landing__page_photo}>
           <div className={styles.radialContainer}></div>
+          <LandingPageDescription />
           <Image
             src={LandingPageBackground}
             alt="Background Image: Violet house"
@@ -17,7 +33,9 @@ export default function Home() {
           />
           <SearchForm />
         </div>
+        <div className={styles.page_gap}></div>
       </section>
     </main>
   );
 }
+//;
